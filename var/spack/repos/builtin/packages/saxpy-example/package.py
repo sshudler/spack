f97b8fb4868fff6407f0ff530f3ac821bd635eee
Sergei Shudler <shudler@gilgamesh.nic.uoregon.edu>
2021-01-07 18:55:05 -0800

    Updated saxpy-example script to support Hip

--- var/spack/repos/builtin/packages/saxpy-example/package.py
+++ var/spack/repos/builtin/packages/saxpy-example/package.py
@@ -23,7 +23,7 @@
 from spack import *
 
 
-class SaxpyExample(CMakePackage, CudaPackage):
+class SaxpyExample(CMakePackage, CudaPackage, ROCmPackage):
     """Simple SAXPY computation example in CUDA, Hip, and OneAPI."""
 
     homepage = "https://github.com/sshudler/saxpy-example"
@@ -34,23 +34,49 @@ class SaxpyExample(CMakePackage, CudaPackage):
 
     version('master', branch='master')
 
-    variant("cuda", default=True, description="Build with Cuda support")
-
-    depends_on('cuda@10.1.0:', when='+cuda')
-
-    # conflicts("+hip", when="+cuda")
+    depends_on('cmake@3.13:', when='+cuda', type='build')
 
     def cmake_args(self):
         spec = self.spec
         options = []
         if '+cuda' in spec:
-            options.append('-DENABLE_CUDA=ON')
-            # options.append("-DCMAKE_CUDA_HOST_COMPILER={0}".format(
-            #                env["SPACK_CXX"]))
-            cuda_arch = spec.variants['cuda_arch'].value
-            if cuda_arch[0] != 'none':
-                options.append('-DCUDA_FLAGS=-arch=sm_{0}'.format(cuda_arch[0]))
+            options.extend([
+                '-DENABLE_CUDA=ON',
+                '-DCUDA_TOOLKIT_ROOT_DIR={0}'.format(spec['cuda'].prefix),
+                '-DCMAKE_CUDA_HOST_COMPILER={0}'.format(env["SPACK_CXX"])])
+            cuda_archs = spec.variants['cuda_arch'].value
+            if 'none' not in cuda_archs:
+                options.append('-DCMAKE_CUDA_FLAGS=-arch=sm_{0}'.format(cuda_arch[0]))
         else:
             options.append('-DENABLE_CUDA=OFF')
 
+        if '+rocm' in spec:
+            options.extend([
+                '-DENABLE_HIP=ON',
+                '-DHIP_ROOT_DIR={0}'.format(spec['hip'].prefix)])
+
+            hip_archs = self.spec.variants['amdgpu_target'].value
+            if 'none' not in hip_archs:
+                archs_str = ",".join(hip_archs)
+                options.append(
+                    '-DHIP_HIPCC_FLAGS=--amdgpu-target={0}'.format(archs_str)
+                )
+        else:
+            options.append('-DENABLE_HIP=OFF')
+
         return options
+
+    def test_target(self, target_exe):
+        reason = "test installation of {0}".format(target_exe)
+        self.run_test(target_exe, ['1'], ['Buffer size: 1048576', 'Max error: 0'],
+                      status=0, installed=True, purpose=reason, skip_missing=False, work_dir='.')
+
+    def test(self):
+        """ Perform smoke test on the installation."""
+
+        if '+cuda' in self.spec:
+            self.test_target('saxpy-cuda')
+
+        if '+rocm' in self.spec:
+            self.test_target('saxpy-hip')
+
